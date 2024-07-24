d3fd33e0e1dbad4f8ade8e27b469d8b9f59ffb80
Vedantjn <vedant.jain.indore@gmail.com>
2024-07-24 19:26:35 +0530

    first commit

--- 3.cpp
+++ 3.cpp
@@ -7,16 +7,16 @@ public:
             if(nums1[i]>=nums2[j]){
                 nums1[i+j+1]=nums1[i] ;
                 i-- ;
-                }
+            }
             else{
                 nums1[i+j+1]=nums2[j];
                 j-- ;
-                }
+            }
         }
         while(j>=0){
                     nums1[j]=nums2[j];
                     j-- ;
-                }      
+        }      
     }
 };
 
